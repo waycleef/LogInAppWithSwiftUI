e0113d026409eff55c21136d2f3cc997e447fb2e
waycleef <khmalikov@gmail.com>
2023-01-18 23:58:18 +0600

    UserDefaults with AppStore

--- LogInAppWithSwiftUI/StorageManager.swift
+++ LogInAppWithSwiftUI/StorageManager.swift
@@ -10,36 +10,38 @@ import Foundation
 class StorageManager {
     static let shared = StorageManager()
     
-    private let userDefaults = UserDefaults.standard
-    private let key = "Key"
-    private let boolKey = "boolKey"
+    @AppStorge(
+    
+//    private let userDefaults = UserDefaults.standard
+//    private let key = "Key"
+//    private let boolKey = "boolKey"
     
     private init () {}
     
-    func save(name: String) {
-        userDefaults.set(name, forKey: key)
-    }
-    
-    func save(bool: Bool) {
-        userDefaults.set(bool, forKey: boolKey)
-    }
-    
-    func fetchDataName() -> String {
-        guard let name = userDefaults.value(forKey: key) as? String else { return ""}
-        return name
-    }
-    
-    func fetchDataBool() -> Bool {
-        guard let bool = userDefaults.value(forKey: boolKey) as? Bool else { return false }
-        return bool
-    }
-    
-    func deleteName() {
-        userDefaults.removeObject(forKey: key)
-    }
-    
-    func resetBool() {
-        userDefaults.removeObject(forKey: boolKey)
-    }
-    
+//    func save(name: String) {
+//        userDefaults.set(name, forKey: key)
+//    }
+//
+//    func save(bool: Bool) {
+//        userDefaults.set(bool, forKey: boolKey)
+//    }
+//
+//    func fetchDataName() -> String {
+//        guard let name = userDefaults.value(forKey: key) as? String else { return ""}
+//        return name
+//    }
+//
+//    func fetchDataBool() -> Bool {
+//        guard let bool = userDefaults.value(forKey: boolKey) as? Bool else { return false }
+//        return bool
+//    }
+//
+//    func deleteName() {
+//        userDefaults.removeObject(forKey: key)
+//    }
+//
+//    func resetBool() {
+//        userDefaults.removeObject(forKey: boolKey)
+//    }
+//
 }
